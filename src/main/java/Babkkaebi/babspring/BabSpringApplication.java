e9f23824eba4063d6a2b341d3c4a64c79b9e8e9b
hyeri0903 <hyerijung0903@gmail.com>
2021-05-25 20:13:55 +0900

    Initial commit

---
+++ src/main/java/Babkkaebi/babspring/BabSpringApplication.java
@@ -0,0 +1,13 @@
+package Babkkaebi.babspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Babkkaebi/babspring/BabSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Babkkaebi.babspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
